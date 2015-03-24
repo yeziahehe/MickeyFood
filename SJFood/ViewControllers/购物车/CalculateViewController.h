5a33db0547786f49a7dd302bf63f7f5ed7d66c7f
yeziahehe <yeziahehe@gmail.com>
2015-03-25 01:38:33 +0800

    结算界面。

---
+++ SJFood/ViewControllers/购物车/CalculateViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculateViewController.h
+//  SJFood
+//
+//  Created by 叶帆 on 15/3/25.
+//  Copyright (c) 2015年 Ye Fan. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CalculateViewController : BaseViewController
+
+@end
---
+++ SJFood/ViewControllers/购物车/CalculateViewController.m
@@ -0,0 +1,23 @@
+//
+//  CalculateViewController.m
+//  SJFood
+//
+//  Created by 叶帆 on 15/3/25.
+//  Copyright (c) 2015年 Ye Fan. All rights reserved.
+//
+
+#import "CalculateViewController.h"
+
+@interface CalculateViewController ()
+
+@end
+
+@implementation CalculateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self setNaviTitle:@"确认订单"];
+}
+
+@end
